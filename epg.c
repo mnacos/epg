6bcba945d3b4879c06212083c3a750e6be866bd2
Michael Nacos <m.nacos@gmail.com>
2009-11-25 14:34:38 +0000

    first epg commit

---
+++ epg.c
@@ -0,0 +1,169 @@
+/*  epg.c -- the C portion of epg -- an Erlang PostgreSQL libpq wrapper
+
+      please read the associated LICENSE file
+	  this program exposes libpq functionality to epg.erl
+*/
+
+/* revision information
+   ----------------------------------------------------------------------
+   Revision: 0.01
+   Date: 2009/11/24 11:32:01
+   Created by: m.nacos@gmail.com
+  ---------------------------------------------------------------------- */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+#include <locale.h>
+#include <libpq-fe.h>
+
+/* gcc -L/usr/local/pgsql8.4.1/lib -I/usr/local/pgsql8.4.1/include -lpq epg.c -o epg */
+
+/* LINE_BUFFER_SIZE is the max number of bytes we allow per line */
+#define LINE_BUFFER_SIZE 8192
+#define HARD_LIMIT 32768
+
+int run;
+PGconn *connection;
+
+/* simplistic die procedure */
+void die (char *msg)
+{
+	fprintf(stdout,"%s\n!error!\n",msg);
+	exit(1);
+}
+
+/* this function simply escapes all double quotes within a string */
+static inline char * esc_dquotes(char * sometext)
+{
+    unsigned int size = strlen(sometext); int d_s = 2 * strlen(sometext);
+    char *newtxt = (char *) malloc ( (d_s + 1) * sizeof(char) ); int k = 0; int i;
+    for(i=0; i<size; i++) {
+		if (sometext[i] == '\"') { strncpy(&newtxt[k++], "\\", 1); }
+		strncpy(&newtxt[k++], &sometext[i], 1);
+	}
+    newtxt[k] = '\0';
+    return newtxt;
+}
+
+/* this procedure handles stdin line by line */
+char * rline()
+{
+	char *buffer = NULL;
+	buffer = malloc(LINE_BUFFER_SIZE+1); buffer[0] = '\0';
+	fgets(buffer, LINE_BUFFER_SIZE, stdin);
+	buffer[LINE_BUFFER_SIZE] = '\0';
+
+	// getting rid of trailing newline only if we have the whole input
+	if (strlen(buffer) != LINE_BUFFER_SIZE-1 || buffer[strlen(buffer)-1] == '\n') {
+		buffer[strlen(buffer)-1] = '\0';
+	}
+	// otheriwse we try to capture the rest, then proceed
+	else {
+		char *more = rline(); int mlen = strlen(more); int nl = strlen(buffer)+mlen+1;
+		if (nl > HARD_LIMIT) {free(buffer);free(more);die("Input str limit reached!");}
+		char *ptr = realloc(buffer,nl*sizeof(char));
+		if (ptr != NULL) { buffer = ptr; strcat(buffer, more); free(more); }
+		else { free(buffer); free(more); die("Out of memory!"); }
+	}
+	int size = strlen(buffer);
+
+	// converting buffer to a wide-character string
+	// wchar_t *conv = (wchar_t *) malloc(size * sizeof(wchar_t));
+	// mbstowcs(conv,buffer,size); free(buffer);
+	return buffer;
+}
+	
+/* this procedure outputs the results of each query */
+void wline (PGresult *res)
+{
+	int response = PQresultStatus(res);
+
+	if (response == PGRES_EMPTY_QUERY) {
+		fprintf(stdout,"!connected!\n");
+		fflush(stdout); return;
+	}
+	else if (response == PGRES_COMMAND_OK) {
+		fprintf(stdout,"%s\n!connected!\n", PQcmdStatus(res));
+		fflush(stdout); return;
+	}
+	else if (response == PGRES_FATAL_ERROR) {
+		fprintf(stdout,"%s!error!\n", PQresultErrorMessage(res));
+		fflush(stdout); return;
+	}
+	else if (response == PGRES_BAD_RESPONSE) {
+		fprintf(stdout,"%s!error!\n", PQresultErrorMessage(res));
+		fflush(stdout); return;
+	}
+
+	int fields = PQnfields(res); int tuples = PQntuples(res); int i,k;
+	fprintf(stdout,"[\n\t{header,\n\t\t[\n\t\t\t");
+	for(k=0;k<fields;k++) {
+		fprintf(stdout,"{\"%s\",%d}",PQfname(res,k), PQftype(res,k));
+		if (k<fields-1) fprintf(stdout,",");
+	}
+	fprintf(stdout,"\n\t\t]\n\t},\n\n");
+	fprintf(stdout,"\t{data,\n\t\t[\n");
+	for(i=0;i<tuples;i++) {
+		fprintf(stdout,"\t\t\t{");
+		for(k=0;k<fields;k++) {
+			if(PQgetisnull(res,i,k)) { fprintf(stdout,"\"%s\"","NULL"); }
+			else {
+				char *esc = esc_dquotes(PQgetvalue(res,i,k));
+				fprintf(stdout,"\"%s\"", esc);
+				free(esc);
+			}
+			if (k<fields-1) fprintf(stdout,",");
+		}
+		if (i<tuples-1) fprintf(stdout,"},\n"); else fprintf(stdout,"}\n");
+	}
+	fprintf(stdout,"\n\t\t]\n\t}\n]\n!eod!\n");
+	fflush(stdout);
+}
+
+/* this procedure submits an SQL query to the server and calls wline */
+char * xline(char *line)
+{
+	// simple interaction with Postgresql
+	if( !abs(strcmp(line,"quit")) ) {run = 0;fprintf(stdout,"!bye!\n");fflush(stdout);}
+	else { PGresult *res = PQexec(connection, line); wline(res); PQclear(res); }
+	return line;
+}
+
+/* this procedure takes a connection string and establishes db connection */
+char * cline(char *line)
+{
+	// simple interaction with Postgresql
+	if( !abs(strcmp(line,"quit")) ) {
+		run = 0; fprintf(stdout,"!bye!\n"); fflush(stdout);
+		return line;	
+	}
+	else { connection = PQconnectdb(line); }
+	if (PQstatus(connection) == CONNECTION_OK) { 
+		char *pusr = PQuser(connection);
+		char *phos = PQhost(connection);
+		char *ppor = PQport(connection);
+		char *pgdb = PQdb(connection);
+		if (phos != NULL) { fprintf(stdout, "%s@%s:%s/%s\n", pusr, phos, ppor, pgdb); }
+		else { fprintf(stdout, "%s@%s:%s/%s\n", pusr, "localhost", ppor, pgdb); }
+		fprintf(stdout,"!connected!\n"); fflush(stdout);
+	}
+	else {
+		fprintf(stdout,"%s!error!\n", PQerrorMessage(connection));
+		PQfinish(connection); connection = NULL;
+		fflush(stdout);
+	}
+	return line;
+}
+
+int main()
+{
+	run = 1; connection = NULL;
+	setlocale(LC_ALL,"");
+	while(run && connection == NULL) { free(cline(rline())); }
+	while(run) { free(xline(rline())); }
+	if(connection != NULL) { PQfinish(connection); }
+	return 0;
+}
+
---
+++ epg.erl
@@ -0,0 +1,157 @@
+%%%--------------------------------------------------------------------- 
+%%% epg -- an Erlang PostgreSQL libpq wrapper
+%%% 
+%%%   please read the associated LICENSE file
+%%%   this code relies on the existence of an epg executable (epg.c)
+%%%---------------------------------------------------------------------
+
+-module(epg).
+-export([start/0,start/1,stop/0,stop/1]).
+-export([exec/1,exec/2,pp/1]).
+
+% revision information
+% ----------------------------------------------------------------------
+-revision('Revision: 0.01 ').
+-created('Date: 2009/11/24 11:32:01 ').
+-created_by('m.nacos@gmail.com').
+% ----------------------------------------------------------------------
+
+start() ->
+    spawn(fun() ->
+		  register(epg, self()),
+		  process_flag(trap_exit, true),
+		  Port = open_port({spawn, "epg"}, [use_stdio, {line, 4096}]),
+		  loop(Port)
+	  end).
+
+start(Id) ->
+    spawn(fun() ->
+		  register(Id, self()),
+		  process_flag(trap_exit, true),
+		  Port = open_port({spawn, "epg"}, [use_stdio, {line, 4096}]),
+		  loop(Port)
+	  end).
+
+stop() ->
+    epg ! stop.
+
+stop(Id) ->
+    Id ! stop.
+
+exec(Msg) ->
+    epg ! {call, self(), Msg},
+    receive
+	{epg, Result} ->
+	    Result
+    end.
+
+exec(Id,Msg) ->
+    Id ! {call, self(), Msg},
+    receive
+	{epg, Result} ->
+	    Result
+    end.
+
+% what we need here is some state... if not connected call must complain!
+loop(Port) ->
+    receive
+		{call, Caller, Msg} ->
+			port_command(Port, [Msg | "\n"]),
+    		case collect_response(Port) of
+        		{data, Response} -> 
+            		Caller ! {epg, response_to_term(Response)};
+        		{Atom, Response} -> 
+					Splat = lists:flatten(lists:map(fun(X)->[X|"\n"] end, Response)),
+            		Caller ! {epg, {Atom, Splat}};
+        		timeout -> 
+            		Caller ! {epg, timeout}
+    		end,
+	    	loop(Port);
+		stop ->
+			port_command(Port, ["quit\n"]),
+    		case collect_response(Port) of
+        		{bye, _} -> exit(normal);
+				Unknown -> exit({bad_response, Unknown})
+	    	end;
+		{'EXIT', Port, Reason} ->
+	    	exit({port_terminated,Reason})
+    end.
+
+collect_response(Port) ->
+    collect_response(Port, [], []).
+
+collect_response(Port, RespAcc, LineAcc) ->
+    receive
+        {Port, {data, {eol, "!eod!"}}} ->
+            {data, lists:concat(lists:reverse(RespAcc))};
+
+        {Port, {data, {eol, "!error!"}}} ->
+            {error, lists:concat(lists:reverse(RespAcc))};
+
+        {Port, {data, {eol, "!connected!"}}} ->
+            {info, lists:concat(lists:reverse(RespAcc))};
+
+        {Port, {data, {eol, "!bye!"}}} ->
+            {bye, lists:concat(lists:reverse(RespAcc))};
+
+        {Port, {data, {eol, Result}}} ->
+            Line = lists:reverse([Result | LineAcc]),
+            collect_response(Port, [ [Line | "\n"] | RespAcc], []);
+
+        {Port, {data, {noeol, Result}}} ->
+            collect_response(Port, RespAcc, [Result | LineAcc])
+
+    %% Prevent the gen_server from hanging indefinitely in case the
+    %% spawned process is taking too long processing the request.
+    	after 72000000 -> 
+           	timeout
+    end.
+
+get_timeout() ->
+    {ok, Value} = application:get_env(epg, timeout),
+    Value.
+
+response_to_term(Response) ->
+	Splat1 = re:replace(Response,"\n","",[multiline, global, {return, list}]),
+	{ok, T, _} = erl_scan:string(Splat1++"."),
+	{ok, Term} = erl_parse:parse_term(T),
+	Term.
+
+% pretty printing function for returned data
+pp(Splat) ->
+	case Splat of 
+		[{header,Header},{data,Data}] ->
+			Hlist = lists:map(fun({Item,_})->Item end, Header),
+			io:format("~s~n",[pp(length(Hlist),Hlist,Data,[],[])]);
+		Splat -> io:format("~p~n",[Splat])
+	end.
+
+% finding the maximum character width for each column
+pp(_,[],Data,Header,L) ->
+	pp(Data,lists:reverse(Header),lists:reverse(L));
+pp(Total,Hlist,Data,Header,L) ->
+	Pos = Total - length(Hlist) + 1,
+	Col = [length(lists:nth(Pos,tuple_to_list(Tuple))) || Tuple <- Data],
+	Max = lists:max(Col),
+	[H|T] = Hlist,
+	pp(Total,T,Data,[H|Header],[Max|L]).
+
+% on to pretty printing in tabular form
+pp(Data,Header,L) ->
+	HString = [lists:concat([" ",string:centre(lists:nth(Num, Header),lists:nth(Num, L)+2)]) || Num <- lists:seq(1,length(Header),1)],
+	Separator = lists:concat([lists:concat(["+", ["-" || _ <- lists:seq(1,Len+2,1)]]) || Len <- L]),
+	% io:format("~s~n",[Separator]),
+	Lines = [
+				[
+					lists:concat([
+						"| ",
+						string:left(
+							lists:nth(Num,tuple_to_list(Tuple)),lists:nth(Num,L)+1
+						)
+					])
+					|| Num <- lists:seq(1, length(tuple_to_list(Tuple)), 1)
+				] || Tuple <- Data
+	],
+	Output = lists:concat([ [ lists:concat([Line, "\n"]) || Line <- Lines ] ]),
+	lists:flatten([HString, "\n", Separator, "\n", Output]).
+
